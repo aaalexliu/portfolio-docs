e7b2901b23dae832931f4dc680569f723d039c20
alexliusq <uilxela7@gmail.com>
2020-12-15 14:21:25 +0800

    breaking out docs into separate folder instead of nested inside margins

---
+++ doczrc.js
@@ -0,0 +1,9 @@
+export default {
+  // base: './projects/margins-me',
+  src: './docs',
+  debug: true,
+  // repository: 'https://github.com/alexliusq/margins-me-frontend',
+  title: "Margins: Technical Overview",
+  description: "How I Built Margins",
+  // link: "ssdsfas"
+}
---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+console.log(process.env.NODE_ENV);
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+  pathPrefix: NODE_ENV === 'production' ? '/projects/margins' : null,
+  siteMetadata: {
+    title: `How I Built Margins`,
+    description: `A technical overview of how I built Margins, one place for all your marginalia`,
+    author: `Alex Liu`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-theme-docz'
+    },
+    // `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+}
