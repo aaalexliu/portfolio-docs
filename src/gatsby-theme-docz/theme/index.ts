18a01a8e23112780db8ccd1eaa88357f2e797ff5
alexliusq <uilxela7@gmail.com>
2020-12-18 12:12:45 +0800

    switch from spread operators to merge

--- src/gatsby-theme-docz/theme/index.ts
+++ src/gatsby-theme-docz/theme/index.ts
@@ -1,26 +1,38 @@
 // import { merge } from 'theme-ui';
 import defaultTheme from 'gatsby-theme-docz/src/theme/index';
-// import { merge } from 'lodash/fp';
+import { merge } from 'lodash/fp';
 
-const theme = {
-  ...defaultTheme,
-  styles: {
-    ...defaultTheme.styles,
-    root: {
-      ...defaultTheme.styles.root,
-      fontSize: 2,
-      // color: 'text',
-      // bg: 'background'
-    },
-    p: {
-      fontSize: 2
-    },
-    ul: {
-      ...defaultTheme.styles.ul,
-      listStyleType: 'disc'
+// const theme = {
+//   ...defaultTheme,
+//   styles: {
+//     ...defaultTheme.styles,
+//     root: {
+//       ...defaultTheme.styles.root,
+//       fontSize: 2,
+//       // color: 'text',
+//       // bg: 'background'
+//     },
+//     p: {
+//       fontSize: 2
+//     },
+//     ul: {
+//       ...defaultTheme.styles.ul,
+//       listStyleType: 'disc'
+//     }
+//   }
+// }
+const theme = merge(defaultTheme,
+  {
+    styles: {
+      root: {
+        fontSize: 2
+      },
+      ul: {
+        listStyleType: 'disc'
+      }
     }
   }
-}
+);
 
 console.log(theme.styles);
 
