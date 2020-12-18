d828f84c0cfadc79fb5f0329de6ad67c4d3ef253
alexliusq <uilxela7@gmail.com>
2020-12-18 11:39:14 +0800

    change font sizing after getting confused between p for padding and p for paragraph...

---
+++ src/gatsby-theme-docz/theme/index.ts
@@ -0,0 +1,28 @@
+// import { merge } from 'theme-ui';
+import defaultTheme from 'gatsby-theme-docz/src/theme/index';
+// import { merge } from 'lodash/fp';
+
+const theme = {
+  ...defaultTheme,
+  styles: {
+    ...defaultTheme.styles,
+    root: {
+      ...defaultTheme.styles.root,
+      fontSize: 2,
+      // color: 'text',
+      // bg: 'background'
+    },
+    p: {
+      fontSize: 2
+    },
+    ul: {
+      ...defaultTheme.styles.ul,
+      listStyleType: 'disc'
+    }
+  }
+}
+
+console.log(theme.styles);
+
+
+export default theme;
